f7212d75f8a2a8bddcb6611e7af92000de9c6d0f
Khush Shah <khushshah51@gmail.com>
2023-08-25 17:21:20 +0530

    added .env and app.config.js

---
+++ app.config.js
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+
+export default{
+  "expo": {
+    "scheme": "acme",
+    "web": {
+      "bundler": "metro"
+    },
+    "plugins": [
+      "expo-router"
+    ],
+    "name": "Chatcord",
+    "slug": "Chatcord",
+    extra: {
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      projectId: process.env.PROJECT_ID,
+      storageBucket: process.env.STORAGE_BUCKET,
+      messagingSenderId: process.env.MESSAGING_SENDER_ID,
+      appId: process.env.APP_ID
+    }
+  }
+}
