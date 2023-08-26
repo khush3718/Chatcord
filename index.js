e0fd8b7d5aa68aaa408c0e6ba26b132e8fc7d21d
Khush Shah <khushshah51@gmail.com>
2023-08-26 18:19:08 +0530

    added files in screen folder and modified index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-import "expo-router/entry";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import Chat from "./screens/chat";
+
+const Stack = createStackNavigator();
+
+function ChatStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Chat" component={Chat} />
+    </Stack.Navigator>
+  );
+}
+
+function RootNavigator() {
+  return (
+    <NavigationContainer>
+      <ChatStack />
+    </NavigationContainer>
+  );
+}
+
+export default function App() {
+  return (
+    <RootNavigator/>
+  );
+}
